33b57b7287bfd6e60f6e0663b2100173a1832069
Ab00dMD <abdalrhmanmohamed94@gmail.com>
2024-12-02 12:06:32 +0200

    change minimax from static to instance class

--- algorithms/minimax.py
+++ algorithms/minimax.py
@@ -1,26 +1,28 @@
 import math
+import time
 from gui.tree import MinimaxNode
 class Minimax:
-    
-    @classmethod
-    def minimax_no_pruning(self, board, depth, maximazing_player):
+    def __init__(self,board,depth,maximazing_player):
+        self.board = board
+        self.depth = depth
+        self.maximazing_player = maximazing_player
+
+    def minimax_no_pruning(self):
         root = MinimaxNode(0,0)
-        best_col, util = self.minimax(board,depth,maximazing_player,pruning=False,parent_node=root,layer=0)
+        best_col, util = self.minimax(self.depth,self.maximazing_player,pruning=False,parent_node=root,layer=0)
         return best_col, util, root
 
-    @classmethod
-    def minimax_pruning(self, board, depth, maximazing_player):
+    def minimax_pruning(self):
         root = MinimaxNode(0,0)
-        best_col, util= self.minimax(board,depth,maximazing_player,pruning=True,parent_node=root,layer=0)
+        best_col, util= self.minimax(self.depth,self.maximazing_player,pruning=True,parent_node=root,layer=0)
         return best_col, util, root
 
-    @classmethod
-    def minimax(self, board, depth, maximizing_player, alpha=-math.inf, beta=math.inf, pruning=True, parent_node=None, layer=0):
-        valid_col = board.get_valid_cols()
-        is_terminal = board.is_terminal_node()
+    def minimax(self, depth, maximizing_player, alpha=-math.inf, beta=math.inf, pruning=True, parent_node=None, layer=0):
+        valid_col = self.board.get_valid_cols()
+        is_terminal = self.board.is_terminal_node()
 
         if depth == 0 or is_terminal:
-            player1_score, player2_score = board.get_heuristic_scores()
+            player1_score, player2_score = self.board.get_heuristic_scores()
             parent_node.score = player1_score - player2_score
             return None, parent_node.score
         
@@ -29,17 +31,17 @@ class Minimax:
             best_col, max_util = None, -math.inf
 
             for col in valid_col:
-                child_board = board.copy() 
-                child_board.add_piece(col)
+                self.board.add_piece(col)
                 child_node = MinimaxNode(0,layer+1)
                 parent_node.add_child(child_node)
 
-                _, util = self.minimax(board=child_board,
-                                       depth=depth-1,
+                _, util = self.minimax(depth=depth-1,
                                        maximizing_player=False,
                                        pruning=pruning,
                                        parent_node=child_node,
                                        layer=layer+1)
+                
+                self.board.remove_piece(col)
                 
                 if util > max_util:
                     best_col, max_util = col, util
@@ -48,7 +50,6 @@ class Minimax:
                 parent_node.best_move = best_col
 
                 if pruning:
-                    print("bonk")
                     alpha = max(alpha, max_util)
 
                     if alpha >= beta:
@@ -61,17 +62,17 @@ class Minimax:
             best_col, min_util = None, math.inf
 
             for col in valid_col:
-                child_board = board.copy() 
-                child_board.add_piece(col)
+                self.board.add_piece(col)
                 child_node = MinimaxNode(0,layer+1)
                 parent_node.add_child(child_node)
                 
-                _, util = self.minimax(board=child_board,
-                                       depth=depth-1,
+                _, util = self.minimax(depth=depth-1,
                                        maximizing_player=True,
                                        pruning=pruning,
                                        parent_node=child_node,
                                        layer=layer+1)
+                
+                self.board.remove_piece(col)
 
                 if util < min_util:
                     best_col, min_util = col, util
@@ -80,7 +81,6 @@ class Minimax:
                 parent_node.best_move = best_col
 
                 if pruning:
-                    print("bonk")
                     beta = min(beta,min_util)
 
                     if alpha >= beta:
