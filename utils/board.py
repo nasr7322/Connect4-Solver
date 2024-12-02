0b82aea72ad44bcd15814d92c07a8f8cb6600a3e
Ahmad Hassan <mi010203269@gmail.com>
2024-12-02 03:07:39 +0200

    optmize heuristic

--- utils/board.py
+++ utils/board.py
@@ -91,7 +91,11 @@ class Board:
           for piece in range(4):
             current_x = col + dc * piece
             current_y = row + dr * piece
-            if not (0 <= current_x < self.width and 0 <= current_y < self.height and self.board[current_y][current_x] == current_player):
+            if not (0 <= current_x < self.width and 0 <= current_y < self.height):
+              score = 0
+              break
+            if self.board[current_y][current_x] != current_player and self.board[current_y][current_x] != Turn.NONE:
+              score = 0
               break
             score *= 4
           score /= 4
